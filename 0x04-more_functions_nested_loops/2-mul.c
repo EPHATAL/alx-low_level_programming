95c01dcc45427cd0544aa75d9521bf09bc53d5e8
EPHATAL <taiwod@rocketmail.com>
2023-08-18 03:10:04 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+
+{
+return (a * b);
+
+}
