8f914c510eb8b4de3b6dfa5920bf3370ca391f2d
EPHATAL <taiwod@rocketmail.com>
2023-08-20 08:48:51 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,23 +10,18 @@ void print_diagonal(int n)
 
 {
 	if (n <= 0)
-	{
 		_putchar('\n');
-
-	}
 	else
 	{
-		int i, j;
+		int k, m;
 
-		for (i = 0; i < n; i++)
+		for (k = 0; k < n; k++)
 		{
-			for (j = 0; j < n; j++)
+			for (m = 0; m < n; m++)
 			{
-				if (j == 1)
-				_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
+				_putchar(' ');
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
