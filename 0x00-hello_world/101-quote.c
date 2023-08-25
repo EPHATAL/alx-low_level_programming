ef140afd1ada0a4f99df744a2d3fd9082159ae56
EPHATAL <taiwod@rocketmail.com>
2023-08-25 04:09:39 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -6,13 +7,12 @@
  * Description: print a quote using write function
  *	write(int fd,const void *buf,size_t count)
  *
- * Return: 1 (error).
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, quo, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
