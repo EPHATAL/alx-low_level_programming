615ff6b2581d14abdab601921801ef1b021cae21
EPHATAL <taiwod@rocketmail.com>
2023-09-02 00:53:09 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - program name
+*@argc: arguments number
+*@argv: arguments array
+*
+*Return: Always 0 (Success)
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+
+return (0);
+
+}
