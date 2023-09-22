ae74088ea28250f1a79ff0441f7b1d835165fd5f
EPHATAL <taiwod@rocketmail.com>
2023-09-22 15:47:19 -0700

    hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,15 @@
-global	main
-	extern	printf
+section .data
+    hello db 'Hello, Holberton',10,0  ; string to be printed
+section .text
+    global main
+    extern printf
 main:
-	mov	edi, format
-	xor	eax, eax
-	call	printf
-	mov	eax, 0
-	ret
-format: db 'Hello, Holberton\n',0
+    push rbp             ; set up stack frame
+    mov rbp, rsp
+    mov edi, hello       ; pass the address of the string as the first argument to printf
+    xor eax, eax         ; clear the EAX register (which is used to return the result of a function call)
+    call printf          ; call the printf function
+    mov rsp, rbp         ; tear down stack frame
+    pop rbp
+    xor eax, eax         ; return 0 to indicate success
+    ret
