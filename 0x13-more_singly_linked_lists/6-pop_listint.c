6fee19dde9c676f52cf31c2207b000217134da21
EPHATAL <taiwod@rocketmail.com>
2023-09-27 15:38:26 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+/**
+*pop_listint - linked list head node deleted
+*@head: linked list first elelment
+*
+*Return: data inside the elements that was deleted,
+*or 0 if the list is empty
+*/
+
+int pop_listint(listint_t **head)
+
+{
+listint_t *temp;
+int num;
+
+if (!head || !head)
+return (0);
+
+num = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+
+return (num);
+}
