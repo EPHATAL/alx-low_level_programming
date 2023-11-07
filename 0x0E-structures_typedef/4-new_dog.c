ec77548d157c12982662dd559008f49d66572d4b
EPHATAL <taiwod@rocketmail.com>
2023-11-07 13:44:17 -0800

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,58 +1,67 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
+
+int_strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
-*_strlen - length of a string
-*@s: string
+*_strlen - Finds the length of a string.
+*@str: The string
 *
-*Return: length of the string
+*Return: The length
 */
-int _strlen(char *s)
+int_strlen(char *str)
 {
-int t;
-
-t = 0;
-
-while (s[t] != '\0')
-{
-t++;
-}
-return (t);
-}
-
-/**
-*_strcpy - string pointer to by src
-*including the terminating null byte (\0)
-*to the buffer pointed to by dest
-*@dest: the buffer in which we copy the string
-*@src: string
-*
-*Return: the pointer to dest
-*/
-char *_strcpy(char *dest, char *src)
-{
-int len, t;
-len = 0;
-while (src[len] != '\0')
-{
+int len = 0;
+while (*str++)
 len++;
-}
-for (t = 0; t < len; t++)
-{
-dest[t] = src[t];
-}
-dest[t] = '\0';
-return (dest);
+return (len);
 }
 
 /**
-*new_dog - a new dog
-*@name: dog name
-*@age dog age
-*@owner: dog owner
+*_strcopy - copies a string pointed to by src, including the
+*terminating null byte, to a buffer pointed to by dest.
+*@dest: The buffer
+*@src: the source
 *
-*Return: pointer to the new dog (Success), NULL otherwise
+*Return: The pointer to dest.
 */
+char *_strcopy(char *dest, char *src)
 {
-dog_t *dog;
-int len1, len2;
+int index = 0;
+
+for (index = 0; src[index]; index++)
+dest[index} = src[index];
+}
+
+/**
+*new_dog - Create a new dog.
+*@name: The name
+*@age: The age
+*@owner: The owner
+*
+*Return: The new
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *doggo;
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
+doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+return (NULL);
+doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (doggo->name == NULL)
+{
+free(doggo->name);
+free(doggo);
+return (NULL);
+}
+doggo->name = _strcopy(doggo->name, name);
+doggo->age = age;
+doggo->owner = _strcopy(doggo->owner, owner);
+
+return (doggo);
+}
