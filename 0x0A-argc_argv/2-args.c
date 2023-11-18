03ec63926c7e76923072ba57befc4a7c9d0c157e
EPHATAL <taiwod@rocketmail.com>
2023-11-18 01:09:32 -0800

    mul

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - print all receives argument
+*@argc: arguments number
+*@argv: arguments array
+*
+*Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+int t;
+for (t = 0; t < argc; t++)
+{
+printf("%s\n", argv[t]);
+}
+
+return (0);
+}
