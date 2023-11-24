2cbed20253fb30af920ef24ea5e1d3156da1158e
EPHATAL <taiwod@rocketmail.com>
2023-11-24 03:26:22 -0800

    unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,22 @@
 #include "main.h"
 
 /**
-*binary_to_unit - function converts a binary number
-*to an unsigned int.
+*binary_to_uint - converts a binary number
 *
-*@b: pointer to string
+*@b: the binary nuber
 *
-*Return: unsigned int
+*Return: the unsigned int form of b
 */
 unsigned int binary_to_uint(const char *b)
 {
-int t, base_two;
-unsigned int td;
-
-td = 0;
+unsigned int num = 0;
 if (!b)
 return (0);
-for (t = 0; b[t] != '\0'; t++)
-	;
-for (t--, base_two = 1; t >= 0; t--, base_two *= 2)
+while (*b)
 {
-if (b[t] != '0' && b[t] != '1')
-{
+if (*b != '0' && *b != '1')
 return (0);
+num = num * 2 + (*b++ - '0');
 }
-if (b[t] & 1)
-{
-td += base_two;
-}
-}
-return (td);
+return (num);
 }
