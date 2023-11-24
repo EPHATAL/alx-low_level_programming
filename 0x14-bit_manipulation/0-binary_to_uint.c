479adbd4ab0c470253bb2e84191e961b89f36085
EPHATAL <taiwod@rocketmail.com>
2023-11-24 01:13:36 -0800

    unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,22 +10,24 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-int t;
+int t, base_two;
 unsigned int td;
 
 td = 0;
 if (!b)
 return (0);
 for (t = 0; b[t] != '\0'; t++)
+	;
+for (t--, base_two = 1, t >= 0; t--, base_two *= 2)
 {
 if (b[t] != '0' && b[t] != '1')
+{
 return (0);
 }
-for (t = 0; b[t] != '\0'; t++)
+if (b[t] & 1)
 {
-td <<= 1;
-if (b[t] == '1')
-td += 1;
+td += base_two;
+}
 }
 return (td);
 }
