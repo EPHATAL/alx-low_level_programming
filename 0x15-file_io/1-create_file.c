890ebe5519040f27746205ac5febc02e5575f702
EPHATAL <taiwod@rocketmail.com>
2023-11-28 06:09:51 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
 *create_file - creates a file
 *
-*@filename: the file
+*@filename: the filename
 *
 *@text_content: A NULL-terminated string
 *
@@ -11,20 +11,29 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int file, result = 1 countn = 0;
+int td;
+int nletters;
+int rwr;
+
 if (!filename)
 return (-1);
-file = open(filename, O_CREAT | O_WRONLY | o_TRUNC, 0600);
-if (file == -1)
+
+td = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+if (td == -1)
 return (-1);
-if (text_content)
-{
-while (text_content[countn])
-countn++;
-result = write(file, text_content, countn);
-}
-if (result == -1)
+
+if (!text_content)
+text_content = "";
+
+for (nletters = 0; text_content[nletters]; nletters++)
+;
+rwr = write(td, text_content, nletters);
+
+if (rwr == -1)
 return (-1);
-close(file);
+
+close(td);
+
 return (1);
 }
