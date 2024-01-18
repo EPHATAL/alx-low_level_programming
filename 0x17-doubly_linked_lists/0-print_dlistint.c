d1af187d1eb2173fa34ead717361a4e341699867
EPHATAL <taiwod@rocketmail.com>
2024-01-18 00:39:30 -0800

    print

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 
 /**
-*print_dlistint - elements of a dlistint_t list
-*@h: head
+*print_dlistint - prints all elements of a 
+*dlistint_t list
+*@h: head of the list
 *Return: the number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
+
 int count;
 
 count = 0;
@@ -23,5 +25,6 @@ printf("%d\n", h->n);
 count++;
 h = h->next;
 }
+
 return (count);
 }
